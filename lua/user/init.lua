e7dc22c9c18d2a505ea361850ff93f6a956edcc6
FukeKazki <kazkichi0906@gmail.com>
2023-02-04 21:31:40 +0900

    不要な設定を消す

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -224,6 +224,8 @@ local config = {
   -- Configure plugins
   plugins = {
     init = {
+      -- stylus用のlsp
+      -- stylusを使わなくなったら消す
       { "iloginow/vim-stylus" },
       -- You can disable default plugins as follows:
       ["goolord/alpha-nvim"] = { disable = true },
@@ -297,54 +299,6 @@ local config = {
           require("windows").setup()
         end,
       },
-      --         `vim.lsp.handlers["textDocument/hover"]` has been overwritten by another plugin?
-      --
-      -- Either disable the other plugin or set `config.lsp.hover.enabled = false` in your **Noice** config.
-      --   - plugin: nvim
-      --   - file: /usr/local/Cellar/neovim/0.8.1/share/nvim/runtime/lua/vim/lsp.lua
-      --   - line: 2250
-      -- {
-      --   "folke/noice.nvim",
-      --   config = function()
-      --     require("noice").setup({
-      --       lsp = {
-      --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      --         override = {
-      --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      --           ["vim.lsp.util.stylize_markdown"] = true,
-      --           ["cmp.entry.get_documentation"] = true,
-      --           -- ["config.lsp.hover.enabled"] = false
-      --         },
-      --         signature = { enabled = false },
-      --         hover = { enabled = false },
-      --       },
-      --       notify = { enabled = false },
-      --       -- you can enable a preset for easier configuration
-      --       presets = {
-      --         -- bottom_search = true, -- use a classic bottom cmdline for search
-      --         command_palette = true, -- position the cmdline and popupmenu together
-      --         long_message_to_split = true, -- long messages will be sent to a split
-      --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      --         lsp_doc_border = false, -- add a border to hover docs and signature help
-      --       },
-      --     })
-      --   end,
-      --   requires = {
-      --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      --     "MunifTanjim/nui.nvim",
-      --     -- OPTIONAL:
-      --     --   `nvim-notify` is only needed, if you want to use the notification view.
-      --     --   If not available, we use `mini` as the fallback
-      --     "rcarriga/nvim-notify",
-      --   },
-      -- },
-      -- We also support a key value style plugin definition similar to NvChad:
-      -- ["ray-x/lsp_signature.nvim"] = {
-      --   event = "BufRead",
-      --   config = function()
-      --     require("lsp_signature").setup()
-      --   end,
-      -- },
     },
     -- All other entries override the require("<key>").setup({...}) call for default plugins
     ["bufferline"] = {
