d65b04c6c41bbd4925e14a20328dce6d62676bc9
FukeKazki <kazkichi0906@gmail.com>
2023-02-11 22:28:28 +0900

    うごくかも

--- lua/myplugins/dps-helloworld/denops/helloworld/main.ts
+++ lua/myplugins/dps-helloworld/denops/helloworld/main.ts
@@ -1,39 +1,53 @@
-import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
 import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+import { SlackAPI } from "https://deno.land/x/deno_slack_api@0.0.8/mod.ts";
+import { Denops, execute, vars } from "./deps.ts";
 
-import { SlackAPI } from "https://deno.land/x/deno_slack_api@0.0.8/mod.ts"
-
-const token = 'xoxb-2268347708272-2237937116486-GVVxr5zKskZYo1A404b7WUzy'
-const channel = 'C027WA7N9T2'
-
-// ...or create a client with options
-const client = SlackAPI(token);
+// options.luaでgに設定してるtoken等を取得する
+export const getBuildOption = async (denops: Denops) => {
+  // const token = await vars.g.get<string>(
+  //   denops,
+  //   "slack_api_token",
+  //   "",
+  // );
+  // const channel = await vars.g.get<string>(
+  //   denops,
+  //   "slack_channel",
+  //   "",
+  // );
+  const token = await vars.environment.get(denops, "SLACK_API_TOKEN", "");
+  const channel = await vars.environment.get(denops, "SLACK_CHANNEL", "");
+  return {
+    token,
+    channel,
+  };
+};
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async echo(text: unknown): Promise<unknown> {
-      // `text` が string 型であることを保証する
       ensureString(text);
       return await Promise.resolve(text);
     },
     // slackに投稿する
     async post(text: unknown): Promise<unknown> {
+      const option = await getBuildOption(denops);
+      const client = SlackAPI(option.token);
+      console.log(option);
       ensureString(text);
       const res = await client.chat.postMessage({
         text,
-        channel,
+        channel: option.channel,
       });
-      return res.ok
-    }
+      return res.ok;
+    },
   };
 
   await execute(
     denops,
-    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`
-  )
+    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+  );
   await execute(
     denops,
-    `command! -nargs=1 Slack echomsg denops#request('${denops.name}', 'post', [<q-args>])`
-  )
-};
+    `command! -nargs=1 Slack echomsg denops#request('${denops.name}', 'post', [<q-args>])`,
+  );
+}
